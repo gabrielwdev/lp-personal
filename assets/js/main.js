75efcd55567640f127fa6a6264e9cdf3fe9a3a8d
Paulo Gabriel Bom Sucesso Machado <paulogabriel.bsm@gmail.com>
2024-01-22 16:59:49 -0300

    section calculate

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,6 +48,59 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== CALCULATE JS ===============*/
+const calculateForm = document.getElementById('calculate-form'),
+      calculateCm = document.getElementById('calculate-cm'),
+      calculateKg = document.getElementById('calculate-kg'),
+      calculateMessage = document.getElementById('calculate-message')
+    
+const calculateBmi = (e) =>{
+    e.preventDefault()
+
+    // Check if the fields have a value
+    if(calculateCm.value === '' || calculateKg.value === ''){
+        // Add and remove color
+        calculateMessage.classList.remove('color-green')
+        calculateMessage.classList.add('color-red')
+
+        // Show message
+        calculateMessage.textContent = 'Fill in the Height and Weight '
+    
+        // Remove message
+        setTimeout(() => {
+            calculateMessage.textContent = ''
+        }, 3000)
+    } else{
+        // BMI Formula
+        const cm = calculateCm.value / 100
+        const kg = calculateKg.value
+        const bmi = Math.round(kg / (cm *cm))
+
+        // Show your health status
+        if(bmi < 18.5){
+            // Add color and display message
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ' + bmi + ' and you are skinny'
+        } else if(bmi < 25){
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ' + bmi + ' and you are healthy'
+        } else{
+            calculateMessage.classList.add('color-green')
+            calculateMessage.textContent = 'Your BMI is ' + bmi + ' and you are overweight'
+        }
+
+        // To clear the field
+        calculateCm.value = ''
+        calculateKg.value = ''
+
+        // Remove message four seconds
+        setTimeout(() => {
+            calculateMessage.textContent = ''
+        }, 4000)
+    }
+}
+
+calculateForm.addEventListener('submit', calculateBmi)
+
 
 
 /*=============== EMAIL JS ===============*/
